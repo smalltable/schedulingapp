e749ae8c150a8fe3b1a82a21dc928fdb335924e6
Pamir <pamirkantar21@gmail.com>
2018-11-17 12:14:56 -0800

    more animations more tabs

--- public/lib/app.js
+++ public/lib/app.js
@@ -8,10 +8,19 @@
 //     });
 //     return valid
 // }
+
+// hamburger rotation
+$(".hamIcon").click(function() {
+  $(this).toggleClass("open")
+});
+
+
 $("#addEvent").dblclick(function() {
   console.log("hey");
   $(".addContainer").toggle("fold", 2500);
 });
+
+
 $("#addEventSubmit").click(function() {
 // Check if all fields are full, if yes then fold it back and send the form field.
 });
@@ -20,3 +29,12 @@ $("#groupEvent").dblclick(function(){
   $("#addEvent").fadeToggle("slow");
   $(".groupContainer").toggle("fold", 2500);
 })
+
+$("#shareCalendar").dblclick(function(){
+  $("#addEvent").fadeToggle("slow");
+  $("#groupEvent").fadeToggle("slow");
+  $(".shareContainer").toggle("fold", 2500);
+})
+
+var ul = document.getElementsByClassName("fList")
+var li = [ul[0].getElementsByTagName("li")];
